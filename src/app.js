d08c4f8434aa30ccf126d26578f58d6beb517a23
mo-ha-amini <84243421+mo-ha-amini@users.noreply.github.com>
2021-05-30 17:45:43 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -20,82 +20,87 @@ hbs.registerPartials(partialsPath)
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'MohammadHasan Amini'
-    })
+  res.render('index', {
+    title: 'Weather',
+    name: 'MohammadHasan Amini',
+  })
 })
 
 app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'MohammadHasan Amini'
-    })
+  res.render('about', {
+    title: 'About Me',
+    name: 'MohammadHasan Amini',
+  })
 })
 
 app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'This is some helpful text.',
-        title: 'Help',
-        name: 'MohammadHasan Amini'
-    })
+  res.render('help', {
+    helpText: 'This is some helpful text.',
+    title: 'Help',
+    name: 'MohammadHasan Amini',
+  })
 })
 
 app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address!',
+    })
+  }
 
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error })
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
-            return res.send({ error })
+          return res.send({ error })
         }
 
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
-
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
         })
-    })
+      })
+    }
+  )
 })
 
 app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
-    }
-
-    console.log(req.query.search)
-    res.send({
-        products: []
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term',
     })
+  }
+
+  console.log(req.query.search)
+  res.send({
+    products: [],
+  })
 })
 
 app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'MohammadHasan Amini',
-        errorMessage: 'Help article not found.'
-    })
+  res.render('404', {
+    title: '404',
+    name: 'MohammadHasan Amini',
+    errorMessage: 'Help article not found.',
+  })
 })
 
 app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'MohammadHasan Amini',
-        errorMessage: 'Page not found.'
-    })
+  res.render('404', {
+    title: '404',
+    name: 'MohammadHasan Amini',
+    errorMessage: 'Page not found.',
+  })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log('Server is up on port : ' + port)
 })
